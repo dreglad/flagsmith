60a742a02f87e0f3d1e91ec9f71b773431a28d3c
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-10-19 13:41:54 +0530

    use redis for user throttle cache

---
+++ api/core/throttling.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.core.cache import caches
+from rest_framework.throttling import UserRateThrottle
+
+
+class AdminRateThrottle(UserRateThrottle):
+    cache = caches[settings.ADMIN_THROTTLE_CACHE]
