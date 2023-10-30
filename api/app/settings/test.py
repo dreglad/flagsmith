94f40d8846549cd17df2587382d5a329219bb3e3
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-10-30 14:31:53 +0530

    [minor]

--- api/app/settings/test.py
+++ api/app/settings/test.py
@@ -3,6 +3,7 @@ from app.settings.common import REST_FRAMEWORK
 
 # We dont want to track tests
 ENABLE_TELEMETRY = False
+
 DEFAULT_THROTTLE_CLASSES = ["core.AdminRateThrottle"]
 REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
     "login": "100/min",
