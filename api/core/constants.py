3d50fa31644be2a92925cb7a4b2aa301f0861783
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-10-30 14:30:57 +0530

    rollback sentry sampler changes

--- api/core/constants.py
+++ api/core/constants.py
@@ -8,15 +8,3 @@ FLOAT = "float"
 FLAGSMITH_SIGNATURE_HEADER = "X-Flagsmith-Signature"
 
 FLAGSMITH_UPDATED_AT_HEADER = "X-Flagsmith-Document-Updated-At"
-
-NON_FUNCTIONAL_ENDPOINTS = ("/health", "")
-
-SDK_ENDPOINTS = (
-    "/api/v1/flags",
-    "/api/v1/identities",
-    "/api/v1/traits",
-    "/api/v1/traits/bulk",
-    "/api/v1/traits/increment-value",
-    "/api/v1/environment-document",
-    "/api/v1/analytics/flags",
-)
